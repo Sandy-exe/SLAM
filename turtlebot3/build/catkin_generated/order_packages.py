3bcdc814750625e370943679a88e0f9095a22474
Sandy-exe <santhoshkumar555001@gmail.com>
2023-09-16 17:01:23 +0530

    commit

---
+++ turtlebot3/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/asus/Documents/SLAM/turtlebot3/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/asus/Documents/RosLearn/catkin_ws/devel;/home/asus/Documents/SLAM/simulation_slam/devel;/opt/ros/noetic'.split(';') if '/home/asus/Documents/RosLearn/catkin_ws/devel;/home/asus/Documents/SLAM/simulation_slam/devel;/opt/ros/noetic' != '' else []
